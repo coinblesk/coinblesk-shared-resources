2be3de5baa2ddc2225ba4b66ee5b711925c6bb43
malib <mehmetali.bekooglu@uzh.ch>
2014-08-21 00:02:08 +0200

    update

--- mbps-shared-resources/src/main/java/ch/uzh/csg/mbps/responseobject/ServerAccountTransferObject.java
+++ mbps-shared-resources/src/main/java/ch/uzh/csg/mbps/responseobject/ServerAccountTransferObject.java
@@ -6,17 +6,31 @@ import ch.uzh.csg.mbps.model.ServerAccount;
 
 public class ServerAccountTransferObject {
 	private ArrayList<ServerAccount> serverAccountList;
-
+	private long numberofSA;
+	
 	public ServerAccountTransferObject() {
 		serverAccountList = new ArrayList<ServerAccount>();
 	}
 
+	public ServerAccountTransferObject(ArrayList<ServerAccount> list, long nofSA) {
+		serverAccountList = new ArrayList<ServerAccount>();
+		numberofSA = nofSA;
+	}
+	
 	public ArrayList<ServerAccount> getServerAccountList() {
 		return serverAccountList;
 	}
 
 	public void setServerAccountList(ArrayList<ServerAccount> list) {
 		this.serverAccountList = list;
+	}
+	
+	public long getNumberOfSA(){
+		return numberofSA;
+	}
+	
+	public void setNumberOfSA(long nofSA){
+		this.numberofSA = nofSA;
 	}
 	
 	public void decode(String payOutRulesTO) {
