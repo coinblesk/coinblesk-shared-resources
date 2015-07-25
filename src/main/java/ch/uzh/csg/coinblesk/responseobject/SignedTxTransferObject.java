2a1d590cdc3c421515dcd24df3f99dab988b94eb
Raphael <r.voellmy@gmail.com>
2015-07-25 16:44:56 +0200

    add signed transaction transfer object, remove target directory from git index

---
+++ src/main/java/ch/uzh/csg/coinblesk/responseobject/SignedTxTransferObject.java
@@ -0,0 +1,23 @@
+package ch.uzh.csg.coinblesk.responseobject;
+
+public class SignedTxTransferObject extends TransferObject {
+    
+    private String signedTx;
+    
+    public SignedTxTransferObject() {}
+    
+    public SignedTxTransferObject(String signedTx) {
+        this.signedTx = signedTx;
+    }
+
+    public String getSignedTx() {
+        return signedTx;
+    }
+
+    public void setSignedTx(String signedTx) {
+        this.signedTx = signedTx;
+    }
+    
+    
+
+}
