24213efee70baa61eddb13f460b7bbe07fa45cd2
tbocek <tom@tomp2p.net>
2016-02-27 19:39:23 +0100

    remove unused dependency

--- src/main/java/com/coinblesk/util/BitcoinUtils.java
+++ src/main/java/com/coinblesk/util/BitcoinUtils.java
@@ -10,7 +10,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.ECKey;
