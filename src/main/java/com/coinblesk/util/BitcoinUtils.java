32b696f13d024f96c2c357ae89119e7f88ad189b
Thomas Bocek <tom@tomp2p.net>
2016-03-10 12:41:18 +0100

    workaround for Guava.sort

--- src/main/java/com/coinblesk/util/BitcoinUtils.java
+++ src/main/java/com/coinblesk/util/BitcoinUtils.java
@@ -22,9 +22,12 @@ import org.bitcoinj.core.Transaction;
 import org.bitcoinj.core.TransactionInput;
 import org.bitcoinj.core.TransactionOutPoint;
 import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.core.Utils;
 import org.bitcoinj.crypto.TransactionSignature;
 import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptBuilder;
+import static org.bitcoinj.script.ScriptBuilder.createP2SHOutputScript;
+import static org.bitcoinj.script.ScriptBuilder.createRedeemScript;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -343,5 +346,21 @@ public class BitcoinUtils {
              tx.addOutput(transactionOutput);
          }
          
+    }
+    
+    public static Script createRedeemScript(int threshold, List<ECKey> pubkeys) {
+        pubkeys = new ArrayList<>(pubkeys);
+        Collections.sort(pubkeys, ECKey.PUBKEY_COMPARATOR);
+        return ScriptBuilder.createMultiSigOutputScript(threshold, pubkeys);
+    }
+    
+    public static Script createP2SHOutputScript(int threshold, List<ECKey> pubkeys) {
+        Script redeemScript = createRedeemScript(threshold, pubkeys);
+        return createP2SHOutputScript(redeemScript);
+    }
+    
+    public static Script createP2SHOutputScript(Script redeemScript) {
+        byte[] hash = Utils.sha256hash160(redeemScript.getProgram());
+        return ScriptBuilder.createP2SHOutputScript(hash);
     }
 }
