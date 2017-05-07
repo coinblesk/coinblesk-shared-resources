cc97ee3bb0955a2206404bb273701b0a715ac73d
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-05-07 15:52:13 +0200

    refactor forex controller & services to support multiple forex vendors

--- src/main/java/com/coinblesk/dto/ForexDTO.java
+++ src/main/java/com/coinblesk/dto/ForexDTO.java
@@ -3,13 +3,15 @@ package com.coinblesk.dto;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.coinblesk.enumerator.ForexCurrency;
+
 import lombok.Data;
 
 @Data
 public class ForexDTO {
 
-	private String currencyFrom;
-	private String currencyTo;
+	private ForexCurrency currencyFrom;
+	private ForexCurrency currencyTo;
 	private BigDecimal rate;
 	private Date updatedAt;
 
