c181f3a8fda2b5e62c324d7343f3a571ede802b6
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-06-15 18:01:51 +0200

    add clientPublicKey & clientPrivateKeyEncrypted to Registration DTO

--- src/main/java/com/coinblesk/dto/UserAccountCreateDTO.java
+++ src/main/java/com/coinblesk/dto/UserAccountCreateDTO.java
@@ -1,8 +1,9 @@
 package com.coinblesk.dto;
 
-import lombok.Data;
-
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import lombok.Data;
 
 @Data
 public class UserAccountCreateDTO {
@@ -12,5 +13,12 @@ public class UserAccountCreateDTO {
 
 	@NotNull
 	private String password;
+
+	@NotNull
+	@Pattern(regexp = "[0-9a-fA-F]{66}", message = "publicKey must be 33 bytes long in hex format (string of length 66)")
+	private String clientPublicKey;
+
+	@NotNull
+	private String clientPrivateKeyEncrypted;
 
 }
