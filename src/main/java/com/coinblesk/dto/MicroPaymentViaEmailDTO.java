ca2b8d3afd37fa2b59389d3603a46f22589df8e1
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-07-23 12:17:44 +0200

    add validation to dto's

--- src/main/java/com/coinblesk/dto/MicroPaymentViaEmailDTO.java
+++ src/main/java/com/coinblesk/dto/MicroPaymentViaEmailDTO.java
@@ -1,12 +1,21 @@
 package com.coinblesk.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class MicroPaymentViaEmailDTO {
 
+	@NotNull
 	private String receiverEmail;
+
+	@NotNull
+	@Min(value = 0L)
 	private Long amount;
+
+	@NotNull
 	private String transaction;
 
 }
--- src/main/java/com/coinblesk/dto/VirtualPaymentViaEmailDTO.java
+++ src/main/java/com/coinblesk/dto/VirtualPaymentViaEmailDTO.java
@@ -1,11 +1,18 @@
 package com.coinblesk.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class VirtualPaymentViaEmailDTO {
 
+	@NotNull
 	private String receiverEmail;
+
+	@Min(value = 0L)
+	@NotNull
 	private Long amount;
-	
+
 }
