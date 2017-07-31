92a31d3e0398f8822e1941e9938c813983d3ee56
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-07-31 20:45:53 +0200

    adapt UnspentTransactionOutputDTO

---
+++ src/main/java/com/coinblesk/dto/UnspentTransactionOutputDTO.java
@@ -0,0 +1,19 @@
+package com.coinblesk.dto;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class UnspentTransactionOutputDTO {
+
+	@NotNull
+	private Long value;
+
+	@NotNull
+	private Integer index;
+
+	@NotNull
+	private String transactionHash;
+
+}
